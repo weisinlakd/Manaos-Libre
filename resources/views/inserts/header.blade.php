75509fad0219713497c471e4f41b2bf43b65d9c4
Luciano <luduena.saavedra@gmail.com>
2020-02-20 19:57:32 -0300

    register MVP

--- resources/views/inserts/header.blade.php
+++ resources/views/inserts/header.blade.php
@@ -78,7 +78,12 @@
                     <a class="dropdown-item" href="perfil.php#settings">Editar perfil</a>
                     <a class="dropdown-item" href="crearPublicacion.php">Crear Publicación</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="cerrarSesion.php">Cerrar Sesión</a>
+                    
+                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">Cerrar Sesión</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                      @csrf
+                    </form>
                   </div>
             </li>
           @endguest
