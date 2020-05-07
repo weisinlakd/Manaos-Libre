f0e97e1b308e946a1852694c734fcb415c76a1d2
Luciano <luduena.saavedra@gmail.com>
2020-05-07 14:59:31 -0300

    deploy v2

--- manaos-libre.sql
+++ manaos-libre.sql
@@ -21,9 +21,9 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `mydb`
 --
-DROP DATABASE IF EXISTS `mydb`;
-CREATE DATABASE IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `mydb`;
+-- DROP DATABASE IF EXISTS `mydb`;
+-- CREATE DATABASE IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+-- USE `mydb`;
 
 -- --------------------------------------------------------
 
